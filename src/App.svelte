639539637d05836a350f86912c3d42588228546f
meldzhaLV <48732283+meldzhaLV@users.noreply.github.com>
2021-01-08 10:36:59 +0200

    Svelte
    Update with Svelte components

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Index from './index.svelte';
+</script>
+
+<!-- OUTPUT -->
+<Index />
+
+
---
+++ src/index.svelte
@@ -0,0 +1,144 @@
+<header class="head">
+    <div class="logo">
+        <img src="https://global-uploads.webflow.com/5e157547d6f791d34ea4e2bf/5e17558f848f82e664c09d67_logo-dark.svg"
+             alt="logo">
+    </div>
+
+    <nav>
+        <ul>
+            <li><a href="index.html">Home </a></li>
+            <li><a href="about.html">About </a></li>
+            <li><a href="news.html">News </a></li>
+            <li><a href="contacts.html">Contact</a></li>
+        </ul>
+    </nav>
+</header>
+
+<!-- HERO REPRESENTATION -->
+<section class="hero">
+    <h1>We learn code</h1>
+    <a href="#">Ali's</a> <span>friendly guides to your biggest coding questions</span>
+</section>
+
+<section class="cta">
+    <a href="#">Get my free ebook about all the things I wish I knew when I was learning to code! >></a>
+</section>
+<!-- END OF REPRESENTATION -->
+
+
+<!-- POST SECTION -->
+<section class="posts">
+    <article>
+        <header>
+            <img src="https://picsum.photos/200/300" alt="">
+            <h2>Introducing the AWS Amplify Admin UI: Create an Application Backend in Clicks</h2>
+            <div class="time-flex">
+                <p><time datetime="2017-02-14">2020-12-12</time></p>
+                <a href="#">Read more..</a>
+            </div>
+        </header>
+
+    </article>
+
+    <article>
+        <header>
+            <img src="https://picsum.photos/200/301" alt="">
+            <h2>Introducing the AWS Amplify Admin UI: Create an Application Backend in Clicks</h2>
+            <div class="time-flex">
+                <p><time datetime="2017-02-14">2020-12-12</time></p>
+                <a href="#">Read more..</a>
+            </div>
+        </header>
+
+    </article>
+    <article>
+        <header>
+            <img src="https://picsum.photos/200/306" alt="">
+            <h2>Introducing the AWS Amplify Admin UI: Create an Application Backend in Clicks</h2>
+            <div class="time-flex">
+                <p><time datetime="2017-02-14">2020-12-12</time></p>
+                <a href="#">Read more..</a>
+            </div>
+        </header>
+
+    </article>
+
+    <article>
+        <header>
+            <img src="https://picsum.photos/200/305" alt="">
+            <h2>Introducing the AWS Amplify Admin UI: Create an Application Backend in Clicks</h2>
+            <div class="time-flex">
+                <p><time datetime="2017-02-14">2020-12-12</time></p>
+                <a href="#">Read more..</a>
+            </div>
+        </header>
+    </article>
+
+
+</section>
+<!-- END OF POST SECTION -->
+
+
+<!-- ABOUT SECTION -->
+<section class="about">
+    <h2>Hi! i'm BOB</h2>
+    <p>I’m a software engineer and teacher. I blog about the things I wish I knew when I was learning to code. You'll find technical tutorials, career advice, and more all right here!</p>
+    <a href="#">
+        <button>PRESS ME</button>
+    </a>
+</section>
+
+<section class="projects">
+    <article>
+        <header>
+            <img src="https://picsum.photos/100/100" alt="">
+            <h2>Project 1</h2>
+            <div class="time-flex-2">
+                <p><time datetime="2017-02-14">2020-12-12</time></p>
+                <a href="#">Read more..</a>
+            </div>
+        </header>
+
+    </article>
+    <article>
+        <header>
+            <img src="https://picsum.photos/100/103" alt="">
+            <h2>Project 1</h2>
+            <div class="time-flex-2">
+                <p><time datetime="2017-02-14">2020-12-12</time></p>
+                <a href="#">Read more..</a>
+            </div>
+        </header>
+
+    </article>
+    <article>
+        <header>
+            <img src="https://picsum.photos/100/102" alt="">
+            <h2>Project 2</h2>
+            <div class="time-flex-2">
+                <p><time datetime="2017-02-14">2020-12-12</time></p>
+                <a href="#">Read more..</a>
+            </div>
+        </header>
+    </article>
+    <article>
+        <header>
+            <img src="https://picsum.photos/100/101" alt="">
+            <h2>Project 2</h2>
+            <div class="time-flex-2">
+                <p><time datetime="2017-02-14">2020-12-12</time></p>
+                <a href="#">Read more..</a>
+            </div>
+        </header>
+    </article>
+</section>
+<!-- END OF ABOUT SECTION -->
+
+
+<!-- FOOTER -->
+<footer>
+    Copyright 	&#169; VALDIS & CO
+</footer>
+<!-- END OF FOOTER -->
+
+
